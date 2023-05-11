9bcbc759026a8ba081d3706561e49e97dcb844e6
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-05-11 16:41:30 +0200

    Last commit before crucial changes

--- client/src/context/collection.context.js
+++ client/src/context/collection.context.js
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const CollectionContext = createContext()
 
 export const CollectionProvider = ({children}) => {
-  const [currentCollection, setCurrentCollection] = useState(null)
+  const [currentCollection, setCurrentCollection] = useState(localStorage.getItem('currentCollection'))
+
+  useEffect(() => {
+    setCurrentCollection(localStorage.getItem('currentCollection'))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('currentCollection', currentCollection);
+  }, [currentCollection]);
+  
 
   return(
     <CollectionContext.Provider value={{currentCollection, setCurrentCollection}}>
@@ -11,4 +20,3 @@ export const CollectionProvider = ({children}) => {
     </CollectionContext.Provider>
   )
 }
-
